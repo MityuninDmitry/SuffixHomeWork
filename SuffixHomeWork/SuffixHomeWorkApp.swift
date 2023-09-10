7852795aeb437568886762f4357590b022b70fa7
Mityunin Dmitry <mity-ml@mail.ru>
2023-09-10 18:01:45 +0500

    Initial Commit

---
+++ SuffixHomeWork/SuffixHomeWorkApp.swift
@@ -0,0 +1,17 @@
+//
+//  SuffixHomeWorkApp.swift
+//  SuffixHomeWork
+//
+//  Created by Dmitry Mityunin on 9/10/23.
+//
+
+import SwiftUI
+
+@main
+struct SuffixHomeWorkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
